949ba57c6bceccc5fd833230905a6bff04df9f87
keremkocak5 <72099354+keremkocak5@users.noreply.github.com>
2025-12-27 22:18:45 +0000

    Refactoring

--- src/main/java/com/giftandgo/converter/service/FileReadable.java
+++ src/main/java/com/giftandgo/converter/service/FileReadable.java
@@ -1,12 +1,9 @@
 package com.giftandgo.converter.service;
 
-import com.giftandgo.converter.validator.Validatable;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Set;
 
 public interface FileReadable<T> {
 
-    T getValidatedFileContent(MultipartFile file, Set<Validatable<String[]>> validators);
+    T getValidatedFileContent(MultipartFile file);
 
 }
--- src/main/java/com/giftandgo/converter/service/impl/FileConverterService.java
+++ src/main/java/com/giftandgo/converter/service/impl/FileConverterService.java
@@ -2,31 +2,23 @@ package com.giftandgo.converter.service.impl;
 
 import com.giftandgo.converter.exception.ConverterRuntimeException;
 import com.giftandgo.converter.model.ConversionLog;
-import com.giftandgo.converter.model.OutcomeContent;
 import com.giftandgo.converter.model.OutcomeFile;
 import com.giftandgo.converter.service.ConversionLogPersistable;
 import com.giftandgo.converter.service.FileConvertable;
 import com.giftandgo.converter.service.FileReadable;
 import com.giftandgo.converter.service.IpValidatable;
-import com.giftandgo.converter.util.FileReadWriteUtil;
-import com.giftandgo.converter.validator.impl.file.FileValidatorFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.List;
-
 @RequiredArgsConstructor
 @Service
 public class FileConverterService implements FileConvertable {
 
     private final ConversionLogPersistable conversionLogService;
-    private final FileReadable<List<OutcomeContent>> fileReadable;
-    private final FileValidatorFactory fileValidatorFactory;
+    private final FileReadable<OutcomeFile> fileReadable;
     private final IpValidatable validateIpService;
 
     @Override
@@ -34,24 +26,14 @@ public class FileConverterService implements FileConvertable {
         long startMoment = System.nanoTime();
         ConversionLog conversionLog = saveConversionLog(ip);
         try {
-            validateIpService.saveIpDetailsAndRunValidationRules(conversionLog, ip);
-            OutcomeFile outcomeFile = getConvertedFile(file);
-            saveExecutionResults(startMoment, conversionLog, HttpStatus.OK); // kerem bu created olsun
+            validateIpService.saveIpDetailsAndRunIpValidationRules(conversionLog, ip);
+            OutcomeFile outcomeFile = fileReadable.getValidatedFileContent(file);
+            saveExecutionResults(startMoment, conversionLog, HttpStatus.OK);
             return outcomeFile;
         } catch (ConverterRuntimeException e) {
             saveExecutionResults(startMoment, conversionLog, e.getErrorCode().getHttpStatus());
             throw e;
         }
-    }
-
-    private OutcomeFile getConvertedFile(MultipartFile file) {
-        List<OutcomeContent> parsedContent = fileReadable.getValidatedFileContent(file, fileValidatorFactory.getValidators());
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        FileReadWriteUtil.write(outputStream, parsedContent);
-        return new OutcomeFile(
-                "OutcomeFile.json",
-                new ByteArrayInputStream(outputStream.toByteArray())
-        );
     }
 
     private ConversionLog saveConversionLog(String ip) {
