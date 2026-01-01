9b1658a63ffdc1119c7d8b22fb13b861a48e9224
keremkocak5 <72099354+keremkocak5@users.noreply.github.com>
2026-01-01 15:31:22 +0000

    Enum fix

--- src/main/java/com/giftandgo/converter/enums/TransportFileValidator.java
+++ src/main/java/com/giftandgo/converter/enums/TransportFileValidator.java
@@ -10,14 +10,14 @@ import java.util.function.BiPredicate;
 @RequiredArgsConstructor
 public enum TransportFileValidator {
 
+    DELIMITER_COUNT(DelimiterCountValidator.INSTANCE, 0, "Delimiter Count", 0),
     UUID(UUIDValidator.INSTANCE, 0, "UUID", 10),
     ID(IDPatternValidator.INSTANCE, 1, "Id", 10),
     NAME(StringNotEmptyLessThan100Validator.INSTANCE, 2, "Name", 10),
     LIKES(StringNotEmptyLessThan100Validator.INSTANCE, 3, "Likes", 10),
     TRANSPORT(StringNotEmptyLessThan100Validator.INSTANCE, 4, "Transport", 10),
     AVG_SPEED(DoubleValidator.INSTANCE, 5, "Average Speed", 10),
-    TOP_SPEED(DoubleValidator.INSTANCE, 6, "Top Speed", 10),
-    DELIMITER_COUNT(DelimiterCountValidator.INSTANCE, 0, "Delimiter Count", 0);
+    TOP_SPEED(DoubleValidator.INSTANCE, 6, "Top Speed", 10);
 
     private final BiPredicate validator;
     private final int associatedColumn;
